d116bbd066deec83a8cd8b54b0d3cde21b5344d2
Jorge Trad <jetradl@gmail.com>
2021-07-18 22:57:03 -0500

    Multiple countries in my currency converter

--- conversor.py
+++ conversor.py
@@ -1,7 +1,37 @@
-pesos = input("¿Cuántos pesos colombianos tienes?: ")
-pesos = float(pesos)
-valor_dolar = 3875
-dolares = pesos / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print("Tienes $" + dolares + " dólares")
+menu = """
+BIENVENIDO AL CONVERSOR DE MONEDAS 💸
+
+1.- PESOS COLOMBIANOS
+2.- PESOS ARGENTINOS
+3.- PESOS MEXICANOS
+
+ELIGE UNA OPCIÓN: """
+
+opcion = int(input(menu))
+
+if opcion == 1:
+    pesos = input("¿Cuántos pesos Colombianos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 3875
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares")
+elif opcion == 2:
+    pesos = input("¿Cuántos pesos Argentinos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 65
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares")
+elif opcion == 3:
+    pesos = input("¿Cuántos pesos Mexicanos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 24
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares")
+else:
+    print('Ingresa una opción correcta')
