88e24d10d295a076cdfc96831966c7fc94326953
Jorge Trad <jetradl@gmail.com>
2021-08-04 14:16:33 -0500

    Store multiple values in one variable: lists

---
+++ listas.py
@@ -0,0 +1,20 @@
+numeros = [1, 3, 6, 8, 9, 45, 90]
+
+objetos = ['Hola', 3, 4.5, True]
+
+objetos[1]
+objetos[0]
+objetos[3]
+objetos[2]
+
+print(objetos)
+
+objetos.append(False)
+objetos.append(1)
+
+objetos.pop(1)
+
+print(objetos)
+
+for elemento in objetos:
+    print(elemento)
